d0ba755ee6e5f3b417240d526ef97de7e4854797
Alexander Serednyakov <aseredniakov@snm-code.ru>
2018-09-04 16:56:51 +0300

    some changes

--- app/src/main/java/com/example/test/testapp/MainActivity.java
+++ app/src/main/java/com/example/test/testapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // some changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
